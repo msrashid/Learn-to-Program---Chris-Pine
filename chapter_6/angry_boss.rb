a3e98786e0bda2f3ab4aa12f99348a09c4708550
Mohammed Rashid <msrashid1995@gmail.com>
2016-06-07 22:11:48 +0000

    Learning centering and string variations

---
+++ chapter_6/angry_boss.rb
@@ -0,0 +1,3 @@
+puts "WHAT DO YOU WANT!!!!!!!!!"
+response=gets.chomp
+puts "WHADDAYA MEAN '" + response.upcase + "'? YOU'RE FIRED!!!!!!!!!"
---
+++ chapter_6/contents.rb
@@ -0,0 +1,5 @@
+line_width = 15
+puts ("Table of Contents").center(50)
+puts (("Chapter 1").ljust(line_width)) + (("Getting Started").center(line_width)) + (("page 1").rjust(line_width))
+puts (("Chapter 2").ljust(line_width)) + (("LOVE").center(line_width)) + (("page 34").rjust(line_width))
+puts (("Chapter 3").ljust(line_width)) + (("LIFE").center(line_width)) + (("page 89").rjust(line_width))
