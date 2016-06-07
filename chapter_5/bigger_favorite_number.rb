0a09127b636345f2c201384806065ecfd1bda1c4
Mohammed Rashid <msrashid1995@gmail.com>
2016-06-07 21:44:30 +0000

    Learning to chomp

---
+++ chapter_5/bigger_favorite_number.rb
@@ -0,0 +1,4 @@
+puts "What is your favorite number?"
+num=gets.chomp
+num=num.to_i+1
+puts "Maybe your favorite number should be: " + num.to_s
---
+++ chapter_5/full_name_greeting.rb
@@ -0,0 +1,8 @@
+puts "Hello what's your first name?"
+first=gets.chomp
+puts "Hello what's your middle name?"
+middle=gets.chomp
+puts "Hello what's your last name?"
+last=gets.chomp
+
+puts "Hello! You're full name is: " + first + " " + middle + " " + last + "!"
