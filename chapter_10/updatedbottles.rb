ffde8a2b1c3c514dce113f5ee57cfa5df42f5076
Mohammed Rashid <msrashid1995@gmail.com>
2016-06-10 11:14:21 +0000

    Updated bottles and much more complicated programs.

--- chapter_10/updatedbottles.rb
+++ chapter_10/updatedbottles.rb
@@ -1,5 +1,7 @@
+x=99
+
 def english_num x
-    @ones_place = ["hello",'one', 'two', 'three','four', 'five', 'six', 'seven', 'eight', 'nine']
+    @ones_place = ["zero",'one', 'two', 'three','four', 'five', 'six', 'seven', 'eight', 'nine']
     @tens_place = ["hello",'ten', 'twenty', 'thirty','forty', 'fifty', 'sixty','seventy', 'eighty', 'ninety']
     @teenagers = ["hello",'eleven', 'twelve', 'thirteen','fourteen', 'fifteen', 'sixteen','seventeen' , 'eighteen', 'nineteen']
     @each=[1000000, 100000, 10000, 1000, 100, 10, 1]
@@ -46,6 +48,8 @@ def ten x
   if(x>=11 && x<=19)
     x=x%10
     print @teenagers[x] + " "
+  elsif (x<10)
+  one(x)
   else 
     z=(x/10)
     print @tens_place[z] + " "
@@ -57,12 +61,35 @@ end
 
 def one x
   c=x/1
+    if(c==0)
+    else
     print @ones_place[c] 
+    end
+end
+
+def bottles x
+while(x!=-1)
+x=x.to_i
+if (x==1)
+print "#{english_num(x)} bottles of beer on the wall, "
+print " #{english_num(x)} bottles of beer. Take one down and pass it around, no more bottles of beer on the wall."
+puts
+elsif (x==0)
+  puts "No more bottles of beer on the wall, no more bottles of beer. 
+Go to the store and buy some more, 99 bottles of beer on the wall."
+else
+	print "#{english_num(x)} bottles of beer on the wall, "
+	print "#{english_num(x)} bottles of beer. Take one down and pass it around, "
+	print "#{english_num(x-1)} bottles of beer on the wall."
+	puts
+end
+x=x-1
+end
 end
 
 puts "Check the problem, type 'end' when finished"
 l = gets.chomp
-english_num(l)
+bottles(l)
 while (l!="end")
 puts 
 puts "Check the problem, type 'end' when finished"
